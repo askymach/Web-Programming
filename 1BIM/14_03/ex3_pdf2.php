441bb8dad673741f241862a382ebe7f581883a21
Arthur Lennon <arthur.dartora.tds2023@gmail.com>
2025-03-15 22:00:42 -0300

    ex3_pdf2.php

--- 1BIM/14_03/ex3_pdf2.php
+++ 1BIM/14_03/ex3_pdf2.php
@@ -1,10 +1,10 @@
 <?php
 $cidades = [
-    ['nome' => 'Foz do Iguaçu', 'habitantes' => '250.000', 'area' => '500km²', 'altitude' => '145m', 'estado' => 'Paraná-PR'],
-    ['nome' => 'Cascavel', 'habitantes' => '300.000', 'area' => '420km²', 'altitude' => '320m', 'estado' => 'Paraná-PR'],
-    ['nome' => 'Chapecó', 'habitantes' => '240.000', 'area' => '120km²', 'altitude' => '620m', 'estado' => 'Santa Catarina-SC'],
-    ['nome' => 'Blumenau', 'habitantes' => '330.000', 'area' => '200km²', 'altitude' => '85m', 'estado' => 'Santa Catarina-SC'],
-    ['nome' => 'Curitiba', 'habitantes' => '1.500.000', 'area' => '300km²', 'altitude' => '850m', 'estado' => 'Paraná-PR']
+    ['nome' => 'FOZ DO IGUAÇU', 'habitantes' => '250.000', 'area' => '500KM²', 'altitude' => '145M', 'estado' => 'PARANÁ-PR'],
+    ['nome' => 'CASCAVEL', 'habitantes' => '300.000', 'area' => '420KM²', 'altitude' => '320M', 'estado' => 'PARANÁ-PR'],
+    ['nome' => 'CHAPECO', 'habitantes' => '240.000', 'area' => '120KM²', 'altitude' => '620M', 'estado' => 'SANTA CATARINA-SC'],
+    ['nome' => 'BLUEMENAU', 'habitantes' => '330.000', 'area' => '200KM²', 'altitude' => '85M', 'estado' => 'SANTA CATARINA-SC'],
+    ['nome' => 'CURITIBA', 'habitantes' => '1.500.000', 'area' => '300KM²', 'altitude' => '850M', 'estado' => 'PARANÁ-PR']
 ];
 
     echo "<table border='1'>";
