ec444e9a2d1f734f93b188c3e4b52e293c9b2ffd
Arthur D. Senna <arthur.dartora.tds2023@gmail.com>
2025-04-11 15:19:26 -0300

    Get

---
+++ 1BIM/11_04/get.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Pàgina aberta!";
+
+$nome = $_GET['nome'];
+
+echo "<br>";
+echo "Bem vindo " . $nome;
+
+$sobrenome = $_GET['sobrenome'];
+echo " " . $sobrenome;
+
+/*
+
+metodo get:
+http://localhost/lingprogweb/1bim/11_04/get.php?nome=Arthur&sobrenome=Senna
+
+
+*/
