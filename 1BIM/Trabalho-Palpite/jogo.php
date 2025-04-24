0dc5ee5123011060901caf2598aad60419c42f0c
Arthur Rhitta <arthur.dartora.tds2023@gmail.com>
2025-04-24 17:42:46 -0300

    Jogo

--- 1BIM/Trabalho-Palpite/jogo.php
+++ 1BIM/Trabalho-Palpite/jogo.php
@@ -1,27 +1,41 @@
 <?php
 require_once 'models/Jogo.php';
-require_once 'models/Palpite.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-if (!isset($_GET['palpite'])) {
-    echo "<h1>Erro: Nenhuma palpite foi inserido na URL! Por favor, insira um palpite!</h1>";
-    echo "<p><a href='index.php'>Voltar à página inicial</a></p>";
-    exit;
-}
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado do Jogo</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
 
-$palpiteEscolhido = $_GET['palpite'];
-$jogo = new Jogo();
-$jogo->escolherPalpiteCorreto();
+<body>
+    <?php
+    if (!isset($_GET['palpite'])) {
+        echo "<h1>Erro: Nenhum palpite informado!</h1>";
+        echo "<p><a href='index.php'>Voltar à página inicial</a></p>";
+        exit;
+    }
 
-if ($jogo->verificarPalpite($palpiteEscolhido)) {
-    $palpite = $jogo->obterPalpitePorId($palpiteEscolhido);
-    echo "<h1>🏆 Parabéns! Você acertou!</h1>";
-    echo "<p>O carro era: <strong>{$palpite->nome}</strong></p>";
-    echo "<img src='images/{$palpite->imagem}' alt='{$palpite->nome}' width='400' />";
-} else {
-    $correto = $jogo->obterPalpiteCorreto();
-    echo "<h1>❌ Que pena, você errou!</h1>";
-    echo "<p>O carro era: <strong>{$correto->nome}</strong></p>";
-    echo "<p>Veja uma imagem borrada dele como dica:</p>";
-    echo "<img src='images/borrada_{$correto->imagem}' alt='Dica' width='400' />";
-    echo "<p><a href='index.php'>Tentar novamente</a></p>";
-}
+    $palpiteEscolhido = $_GET['palpite'];
+    $jogo = new Jogo();
+    $jogo->escolherPalpiteCorreto();
+
+    if ($jogo->verificarPalpite($palpiteEscolhido)) {
+        $palpite = $jogo->obterPalpitePorId($palpiteEscolhido);
+        echo "<h1>🏆 Parabéns! Você acertou!</h1>";
+        echo "<p>O carro era: <strong>{$palpite->nome}</strong></p>";
+        echo "<img src='imagens/{$palpite->imagem}' alt='{$palpite->nome}' />";
+    } else {
+        $correto = $jogo->obterPalpiteCorreto();
+        echo "<h1>❌ Que pena, você errou!</h1>";
+        echo "<p>O carro correto era: <strong>{$correto->nome}</strong></p>";
+        echo "<p>Veja uma imagem borrada dele como dica:</p>";
+        echo "<img src='images/borrada_{$correto->imagem}' alt='Dica' />";
+        echo "<p><a href='index.php'>Tentar novamente</a></p>";
+    }
+    ?>
+</body>
+
+</html>
